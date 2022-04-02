5739438f2bd1079489b96024e29e9d10c072145f
James Herdman <james.herdman@gmail.com>
2022-04-02 09:30:26 -0400

    Basic implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [postItList, addPostIt] = useState([]);
+
+  function insertPostIt(newPostItText) {
+    const nextId = postItList.length + 1;
+    const newPostItList = [...postItList, { id: nextId, text: newPostItText }];
+    return addPostIt(newPostItList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div>
+            <PostItForm insertPostIt={insertPostIt} />
+        </div>
+
+        <div>
+            <PostItList items={postItList} />
+        </div>
+      </main>
     </div>
   );
+}
+
+function PostItForm(props) {
+    const [newPostItText, setPostItText] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if ( newPostItText?.length > 0) {
+            props.insertPostIt(newPostItText);
+            setPostItText('');
+        }
+    }
+
+    const handleChange = (event) => {
+        const { target: { value } } = event;
+        setPostItText(value);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <textarea value={newPostItText} onChange={handleChange} />
+            <button type="submit">Save</button>
+        </form>
+    )
+}
+
+function PostItList(props) {
+    if (!props.items?.length) {
+        return (<p>No items</p>);
+    }
+
+    const { items } = props;
+
+    const postItItems = items.map(item => {
+        return (<PostItItem text={item.text} key={item.id} />);
+    });
+
+    return (<ul>{postItItems}</ul>);
+}
+
+function PostItItem(props) {
+    return (
+        <li>{props.text}</li>
+    )
 }
 
 export default App;
